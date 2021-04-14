81c0a9d4870d56230fc8571e1e4cbb8859510d30
sino.shangzy <zeyu.shang@springs-capital.com>
2021-04-14 10:42:07 +0800

    [项目初始化] <add> 项目初始化

---
+++ src/main/java/com/matmo/mtadmin/MtadminApplication.java
@@ -0,0 +1,13 @@
+package com.matmo.mtadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matmo/mtadmin/MtadminApplicationTests.java
@@ -0,0 +1,12 @@
+package com.matmo.mtadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtadminApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
